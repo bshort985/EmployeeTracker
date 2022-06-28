d511f7bb5825721bbac9777e0b6d14ef82169e30
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-06-28 00:11:02 -0400

    finished add employee, role, and department

--- db/index.js
+++ db/index.js
@@ -9,25 +9,38 @@ class DB {
     }
 
      // formatted table showing department names and department ids
-  showAllDepartments() {
-    return this.connection.promise().query(
-      "SELECT department.id, department.name FROM department;"
-    );
-  }
-  
-    // Join job title, role id, the department that role belongs to, and the salary for that role
-  showAllRoles() {
-    return this.connection.promise().query(
-      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
-    );
-  }
+    showAllDepartments() {
+        return this.connection.promise().query(
+        "SELECT department.id, department.name FROM department;"
+        );
+    }
 
-// Join employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    // Join job title, role id, the department that role belongs to, and the salary for that role
+    showAllRoles() {
+        return this.connection.promise().query(
+        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    // Join employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
  
     showAllEmployees() {
         return this.connection.promise().query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
+    }
+
+      // crate department
+    createDepartment(department) {
+        return this.connection.promise().query("INSERT INTO department SET ?", department);
+    }
+     // create role
+    createRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
+    }
+    // create employee
+    createEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
     }
 
 };
--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function init() {
 // Create prompts
 
 
-// add a department
-// add a role 
-// add an employee
+
+
+
 // update an employee role
 // exit the application
 
@@ -39,24 +39,20 @@ function loadPrompts() {
                     value: "SHOW_ALL_EMPLOYEES"
                 },
                 {
-                    name: "Add a department",
-                    value: "add function"
+                    name: "Add department",
+                    value: "ADD_DEPARTMENT"
                 },
                 {
-                    name: "Add a role",
-                    value: "add function"
+                    name: "Add role",
+                    value: "ADD_ROLE"
                 },
                 {
-                    name: "Add a employee",
-                    value: "add function"
-                },
-                {
-                    name: "Update employee role",
-                    value: "add function"
+                    name: "Add employee",
+                    value: "ADD_EMPLOYEE"
                 },
                 {
                     name: "Exit",
-                    value: "add function"
+                    value: "EXIT"
                 }
             ]
         }
@@ -72,6 +68,17 @@ function loadPrompts() {
             case "SHOW_ALL_EMPLOYEES":
                 allEmployees();
                 break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+                default:
+                exit();
         }
     })
 
@@ -108,3 +115,133 @@ function allEmployees() {
         console.table(employees);
     }).then(() => loadPrompts());
 }
+
+// add a department
+
+function addDepartment() {
+    prompt([
+      {
+        name: "name",
+        message: "What departmenmt would you like to add?"
+      }
+    ])
+      .then(res => {
+        let name = res;
+        db.createDepartment(name)
+          .then(() => console.log(`${name.name} added to the database`))
+          .then(() => loadPrompts())
+      })
+  }
+
+  // add a role 
+
+  function addRole() {
+    db.showAllDepartments()
+      .then(([rows]) => {
+        let departments = rows;
+        const departmentChoices = departments.map(({ id, name }) => ({
+          name: name,
+          value: id
+        }));
+  
+        prompt([
+          {
+            name: "title",
+            message: "What is the role?"
+          },
+          {
+            name: "salary",
+            message: "What is the salary?"
+          },
+          {
+            type: "list",
+            name: "department_id",
+            message: "Which department does the role belong to?",
+            choices: departmentChoices
+          }
+        ])
+          .then(role => {
+            db.createRole(role)
+              .then(() => console.log(`Added ${role.title} to the database`))
+              .then(() => loadPrompts())
+          })
+      })
+  }
+  
+  
+
+  // add an employee
+
+  function addEmployee() {
+    prompt([
+      {
+        name: "first_name",
+        message: "Enter employee's first name"
+      },
+      {
+        name: "last_name",
+        message: "Enter employee's last name?"
+      }
+    ])
+      .then(res => {
+        let firstName = res.first_name;
+        let lastName = res.last_name;
+  
+        db.showAllRoles()
+          .then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({ id, title }) => ({
+              name: title,
+              value: id
+            }));
+  
+            prompt({
+              type: "list",
+              name: "roleId",
+              message: "Select emplyees role",
+              choices: roleChoices
+            })
+              .then(res => {
+                let roleId = res.roleId;
+  
+                db.showAllEmployees()
+                  .then(([rows]) => {
+                    let employees = rows;
+                    const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+                      name: `${first_name} ${last_name}`,
+                      value: id
+                    }));
+  
+                    managerChoices.unshift({ name: "None", value: null });
+  
+                    prompt({
+                      type: "list",
+                      name: "managerId",
+                      message: "Select employees manager",
+                      choices: managerChoices
+                    })
+                      .then(res => {
+                        let employee = {
+                          manager_id: res.managerId,
+                          role_id: roleId,
+                          first_name: firstName,
+                          last_name: lastName
+                        }
+  
+                        db.createEmployee(employee);
+                      })
+                      .then(() => console.log(
+                        `${firstName} ${lastName} added to the database`
+                      ))
+                      .then(() => loadPrompts())
+                  })
+              })
+          })
+      })
+  };
+
+  // Exit 
+function exit() {
+    console.log("Goodbye!");
+    process.exit();
+  }
