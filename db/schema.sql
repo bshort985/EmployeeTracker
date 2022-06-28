0d0473c42d1f9701baf0e7c1c8e54be6f7e57e56
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-06-27 20:02:51 -0400

    finished initial schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,24 @@ USE employees;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) UNIQUE NOT NULL
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) UNIQUE NOT NULL,
+    title VARCHAR(100) NOT NULL,
     salary DECIMAL NOT NULL,
-    dept_id INT NOT NULL 
+    department_id INT NOT NULL,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,36 @@
 use employees;
 
--- Department Table
+-- Department
 INSERT INTO department (name)
     VALUES 
     ("Sales"),
     ("Logistics"),
     ("IT"),
     ("Procument"),
-    ("Assambly")
+    ("Assambly");
 
--- Role table
-    INSERT INTO role (dept_id, title, salary)
+-- Role one manager and one subordinate
+    INSERT INTO role (department_id, title, salary)
     VALUES
     (1, "Account Manager", 105000),
-    (2, "Transportation Coordinator", 65000),
+    (1, "Salesperson", 65000),
+    (2, "Transportation Manager", 100000),
+    (2, "Tansportation Coordinator", 65000),
+    (3, "Technology Manager", 200000),
     (3, "Database Engineer", 150000),
     (4, "Purchasing Lead", 72000),
-    (5, "Floor Supervisor", 68000)
+    (4, "Procument Specialist", 52000),
+    (5, "Floor Supervisor", 68000),
+    (5, "Line Associate", 45000);
+
+    -- Employee link the manager to the employee using ID's
+    INSERT INTO employee(first_name, last_name, role_id, manager_id)
+    VALUES
+    ("William", "Butcher", 1, NULL),
+    ("Hugh", "Cambell", 2, 1),
+    ("Annie", "January", 3, NULL),
+    ("Melvin", "Milk", 4, 3),
+    ("Wade", "Wilson", 5, NULL),
+    ("Dan", "Man", 6, 5),
+    ("Guy", "Dude", 7, NULL),
+    ("Gunther", "O'Dim", 8, 7);
